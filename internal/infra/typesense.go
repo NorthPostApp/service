cc5b724804187ee32f91f02caf266faaf3dd4a3d
akane9506 <akane19950611@gmail.com>
2026-04-14 14:12:48 -0700

    add typesense sync to address create/update/delete flow

--- internal/infra/typesense.go
+++ internal/infra/typesense.go
@@ -1,8 +1,11 @@
 package infra
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log/slog"
+	"north-post/service/internal/domain/v1/models"
 	"os"
 
 	"github.com/typesense/typesense-go/v4/typesense"
@@ -11,7 +14,15 @@ import (
 
 type TypesenseClient struct {
 	Client *typesense.Client
+	logger *slog.Logger
 }
+
+type TypesenseSyncMode int
+
+const (
+	TypesenseAdd TypesenseSyncMode = iota
+	TypesenseDelete
+)
 
 func NewTypesenseClient(logger *slog.Logger) (*TypesenseClient, error) {
 	typesenseURL := os.Getenv("TYPESENSE_URL")
@@ -24,7 +35,7 @@ func NewTypesenseClient(logger *slog.Logger) (*TypesenseClient, error) {
 		typesense.WithAPIKey(apiKey),
 	)
 	logger.Info("Typesense client initialized successfully", "server", typesenseURL)
-	return &TypesenseClient{Client: client}, nil
+	return &TypesenseClient{Client: client, logger: logger}, nil
 }
 
 // The typesense collection schema should be the same as this struct
@@ -35,7 +46,7 @@ type TypesenseAddressRecord struct {
 	Tags       []string `json:"tags"`
 }
 
-func GetTypesenseAddressCollectionSchema(name string) *api.CollectionSchema {
+func (c *TypesenseClient) GetAddressCollectionSchema(name string) *api.CollectionSchema {
 	return &api.CollectionSchema{
 		Name: name,
 		Fields: []api.Field{
@@ -45,4 +56,113 @@ func GetTypesenseAddressCollectionSchema(name string) *api.CollectionSchema {
 			{Name: "tags", Type: "string[]"},
 		},
 	}
+}
+
+func (c *TypesenseClient) CreateAddressRecord(addressItem *models.AddressItem) TypesenseAddressRecord {
+	return TypesenseAddressRecord{
+		ID:         addressItem.ID,
+		Name:       addressItem.Name,
+		BriefIntro: addressItem.BriefIntro,
+		Tags:       addressItem.Tags,
+	}
+}
+
+type SyncDatabaseResult struct {
+	Total   int
+	Success int
+	Failed  int
+}
+
+func (c *TypesenseClient) SyncAddressDatabase(
+	ctx context.Context,
+	collectionName string,
+	documents []interface{}) (*SyncDatabaseResult, error) {
+	// first drop the entire collection to avoid mismatch records
+	_, err := c.Client.Collection(collectionName).Delete(ctx)
+	if err != nil {
+		var httpErr *typesense.HTTPError
+		if !errors.As(err, &httpErr) || httpErr.Status != 404 {
+			c.logger.Error("failed to delete typesense collection when syncing",
+				"collectionName", collectionName,
+				"error", err,
+			)
+			return nil, fmt.Errorf("failed to delete typesense collection when syncing: %w", err)
+		}
+	}
+	// create a new collection
+	schema := c.GetAddressCollectionSchema(collectionName)
+	_, err = c.Client.Collections().Create(ctx, schema)
+	if err != nil {
+		c.logger.Error(
+			"failed to create typesense collection",
+			"collectionName", collectionName,
+			"error", err,
+		)
+		return nil, fmt.Errorf("failed to create typesense collection: %w", err)
+	}
+	// directly return 0 records if no documents were found
+	if len(documents) == 0 {
+		return &SyncDatabaseResult{}, nil
+	}
+	// sync documents
+	action := api.Create
+	params := &api.ImportDocumentsParams{Action: &action}
+	results, err := c.Client.Collection(collectionName).
+		Documents().
+		Import(ctx, documents, params)
+	if err != nil {
+		c.logger.Error(
+			"failed to import documents to typesense",
+			"collectionName", collectionName,
+			"error", err,
+		)
+		return nil, fmt.Errorf("failed to import documents to typesense: %w", err)
+	}
+	// Tally results
+	success := 0
+	for _, result := range results {
+		if result.Success {
+			success++
+		} else {
+			c.logger.Warn(
+				"failed to sync document to typesense",
+				"id", result.Id,
+				"error", result.Error,
+			)
+		}
+	}
+	return &SyncDatabaseResult{
+		Total:   len(results),
+		Success: success,
+		Failed:  len(results) - success,
+	}, nil
+}
+
+func (c *TypesenseClient) UpsertAddressData(
+	ctx context.Context, collectionName string, addressItem *models.AddressItem) {
+	record := c.CreateAddressRecord(addressItem)
+	_, err := c.Client.
+		Collection(collectionName).
+		Documents().
+		Upsert(ctx, record, &api.DocumentIndexParameters{})
+	if err != nil {
+		c.logger.Warn("failed to upsert document",
+			"collectionName", collectionName,
+			"id", addressItem.ID,
+			"error", err,
+		)
+	}
+}
+
+func (c *TypesenseClient) DeleteAddressData(
+	ctx context.Context, collectionName string, addressID string) {
+	_, err := c.Client.Collection(collectionName).Document(addressID).Delete(ctx)
+	if err != nil {
+		c.logger.Warn("failed to delete document",
+			"collectionName", collectionName,
+			"id", addressID,
+			"error", err,
+		)
+	}
+
 }
